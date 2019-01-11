7a3fd12dcf14b80bdfeb40c7fbefac1110285046
Steve Miller <ben.el.elyon@gmail.com>
2019-01-10 23:40:56 -0500

    Complete connecting Video List and Player components

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,56 +1,12 @@
 import VideoListEntry from './VideoListEntry.js';
 
-class VideoList extends React.Component {
-  // props.onClickDisplayVideo <- this references the arg passed in from app.jsx
-  constructor(props) {
-    super(props);
-  }
-
-  // handleDisplayVideo(video) {
-  //   console.log(this);
-  //   this.props.onClickDisplayVideo(video);
-  // }
-
-  render() {
-    return (
-      <div className="video-list">
-        {this.props.videos.map((video) =>
-          <VideoListEntry key={video.id.videoId} video={video} onClickDisplayVideo={this.props.onClickDisplayVideo} />
-        )}
-      </div>
-    )
-  }
-}
-
-// SAVE for stateless functional test
-// var VideoList = (props) => (
-//   var handleDisplayVideo = (video) => {
-//     props.onClickDisplayVideo(video);
-//   }
-//   return(
-//     <div className="video-list">
-//       {props.videos.map((video) =>
-//         <VideoListEntry key={video.id.videoId} video={video} onClickDisplayVideo={handleDisplayVideo.bind(this)} />
-//       )}
-//     </div>
-//   )
-// )
-
-
-// var handleDisplayVideo = (video, props = VideoList.props) => {
-//   console.log(this);
-//   props.onClickDisplayVideo(video);
-// };
-
-// var VideoList = (props) => (
-//   // props.onClickDisplayVideo <- this references the arg passed in from app.jsx
-//   <div className="video-list">
-//     {props.videos.map((video) =>
-//       <VideoListEntry key={video.id.videoId} video={video} onClickDisplayVideo={props.onClickDisplayVideo} />
-//     )}
-
-//   </div>
-// );
+var VideoList = (props) => (
+  <div className="video-list">
+    {props.videos.map((video) =>
+      <VideoListEntry key={video.id.videoId} video={video} onClickDisplayVideo={props.onClickDisplayVideo} />
+    )}
+  </div>
+);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
