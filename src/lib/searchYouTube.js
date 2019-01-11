95f423bd2366a8ef5f55afb75567b8a8f983d578
Steve Miller <ben.el.elyon@gmail.com>
2019-01-11 00:11:09 -0500

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = (options, callback, errorCB) => {
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      maxResults: options.max,
+      q: options.query,
+      type: 'video',
+      key: options.key,
+      videoEmbeddable: true
+    },
+    success: function(data) {
+      const videoData = data.items;
+      callback(videoData);
+    },
+    error: errorCB || function (error) {
+      console.error('recastly: Failed to fetch videos', error);
+    }
+  });
 };
 
 export default searchYouTube;
